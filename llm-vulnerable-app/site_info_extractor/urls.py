b4079d3fe507ae48317383a007f6846a4e45014e
Roy <roy.dar.mail@gmail.com>
2024-06-01 20:12:12 +0300

    Add support for asking questions on sites - minor changes

--- llm-vulnerable-app/site_info_extractor/urls.py
+++ llm-vulnerable-app/site_info_extractor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import AskQuestionOnSiteView
+from .views import AskQuestionOnSiteRagView, AskQuestionOnSiteRetrieverView
 
 urlpatterns = [
-    path('ask/', AskQuestionOnSiteView.as_view(), name='site_question'),
+    path('ask-with-retriever/', AskQuestionOnSiteRetrieverView.as_view(), name='site_question_retriever'),
+    path('ask-with-rag/', AskQuestionOnSiteRagView.as_view(), name='site_question_rag'),
 ]
--- llm-vulnerable-app/site_info_extractor/views.py
+++ llm-vulnerable-app/site_info_extractor/views.py
@@ -13,7 +13,7 @@ _QUESTION_INSTRUCTION_RAG = 'You are a website reader. Answer a question about t
 _logger = logging.getLogger(__name__)
 
 
-class AskQuestionOnSiteView(APIView):
+class AskQuestionOnSiteRetrieverView(APIView):
     queryset = Transaction.objects.none()
 
     # noinspection PyMethodMayBeStatic
@@ -26,23 +26,50 @@ class AskQuestionOnSiteView(APIView):
         if question is None or len(question) == 0:
             raise ValidationError('Invalid question')
 
-        use_rag_str = request.GET.get('use_rag', 'false')
-        use_rag = use_rag_str.lower() == 'true' or use_rag_str == '1'
-
         prompt_args = {"url": site_url, "query": question}
-        if use_rag:
-            instruction = _QUESTION_INSTRUCTION_RAG
-        else:
-            instruction = _QUESTION_INSTRUCTION_RETRIEVER
+        instruction = _QUESTION_INSTRUCTION_RETRIEVER
+        use_embeddings_str = request.GET.get('use_embeddings', 'false')
+        use_embeddings = use_embeddings_str.lower() == 'true' or use_embeddings_str == '1'
 
         answer = None
         error = None
         try:
             llm_helper = LLmHelper()
-            if use_rag:
-                answer = llm_helper.answer_question_on_web_page_with_rag(instruction, prompt_args)
-            else:
-                answer = llm_helper.answer_question_on_web_page_with_retriever(instruction, prompt_args)
+            answer = llm_helper.answer_question_on_web_page_with_retriever(instruction, prompt_args,
+                                                                           embedding=use_embeddings)
+        except Exception as e:
+            _logger.exception("Error while answering the question")
+            error = repr(e)
+        return JsonResponse({
+            "prompt": instruction,
+            "prompt_args": prompt_args,
+            "use_embeddings": use_embeddings,
+            "answer": answer,
+            "error": error
+        }, json_dumps_params={"default": lambda x: vars(x)})
+
+
+class AskQuestionOnSiteRagView(APIView):
+    queryset = Transaction.objects.none()
+
+    # noinspection PyMethodMayBeStatic
+    def get(self, request):
+        site_url = request.GET.get('site_url', '')
+        if site_url is None or len(site_url) == 0:
+            raise ValidationError('Invalid site URL')
+
+        question = request.GET.get('question', '')
+        if question is None or len(question) == 0:
+            raise ValidationError('Invalid question')
+
+        prompt_args = {"url": site_url, "query": question}
+        instruction = _QUESTION_INSTRUCTION_RAG
+
+        answer = None
+        error = None
+        try:
+            llm_helper = LLmHelper()
+            answer = llm_helper.answer_question_on_web_page_with_rag(instruction, prompt_args)
         except Exception as e:
             _logger.exception("Error while answering the question")
             error = repr(e)
